6ce536f4e0590b41c0af80c213d91c670e088a81
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-31 16:34:50 +0900

    초기 커밋.

---
+++ day37_01/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace day37_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BTN_Delete = new System.Windows.Forms.Button();
+            this.INPUT_Delete = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(443, 278);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // BTN_Delete
+            // 
+            this.BTN_Delete.Location = new System.Drawing.Point(361, 296);
+            this.BTN_Delete.Name = "BTN_Delete";
+            this.BTN_Delete.Size = new System.Drawing.Size(94, 27);
+            this.BTN_Delete.TabIndex = 1;
+            this.BTN_Delete.Text = "삭제";
+            this.BTN_Delete.UseVisualStyleBackColor = true;
+            this.BTN_Delete.Click += new System.EventHandler(this.BTN_Delete_Click);
+            // 
+            // INPUT_Delete
+            // 
+            this.INPUT_Delete.Location = new System.Drawing.Point(71, 296);
+            this.INPUT_Delete.Name = "INPUT_Delete";
+            this.INPUT_Delete.Size = new System.Drawing.Size(284, 27);
+            this.INPUT_Delete.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nid: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.INPUT_Delete);
+            this.Controls.Add(this.BTN_Delete);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BTN_Delete;
+        private System.Windows.Forms.TextBox INPUT_Delete;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ day37_01/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace day37_01
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter DA;
+        SqlConnection conn;
+        DataSet Dset;
+        string connString = "server = .\\SQLEXPRESS; database = test; uid = sa; password = alencia";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = connString;
+            Dset = new DataSet();
+            DA = new SqlDataAdapter("SELECT * FROM testTable02", conn);
+            DA.Fill(Dset, "testTable02");
+            dataGridView1.DataSource = Dset.Tables[0];
+                }
+
+        private void BTN_Delete_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            DA.DeleteCommand = new SqlCommand("DELETE FROM [testTable02] WHERE Nid = @Nid");
+            DA.DeleteCommand.Connection = conn;
+            DA.DeleteCommand.Parameters.Add("@Nid", SqlDbType.Int, 12, "Nid");
+            DA.DeleteCommand.Parameters["@Nid"].Value = int.Parse(INPUT_Delete.Text);
+            DA.DeleteCommand.ExecuteNonQuery();
+
+            Dset.Clear();
+            DA.Fill(Dset, "testTable02");
+            dataGridView1.DataSource = Dset.Tables[0];
+            conn.Close();
+            DA.DeleteCommand.Dispose();
+        }
+    }
+}
